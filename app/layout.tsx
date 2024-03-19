8522825b71d1edbe49c96525214ed3139cdde462
filtboris <159777664+filtboris@users.noreply.github.com>
2024-03-19 18:24:58 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ export const metadata: Metadata = {
     title: `${SITENAME} Video Bokep Indo Jepang Barat Viral Terbaru`,
     description: `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
     metadataBase: new URL("https://bokeptoge.pages.dev"),
-    images: https://1.bp.blogspot.com/-0Lzrb1wLbUo/YRNiLprxivI/AAAAAAAAAqo/U4x2LKpxxVcEqlq1iezVoPRU_AxrmQIFQCNcBGAsYHQ/s0/AV%2BThumbnail.webp,
     alternates: {
         canonical: `/`,
     },
@@ -19,6 +18,7 @@ export const metadata: Metadata = {
         title: `${SITENAME} Video Bokep Indo Jepang Barat Viral Terbaru`,
         description: `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
         url: `/`,
+        images: `https://1.bp.blogspot.com/-0Lzrb1wLbUo/YRNiLprxivI/AAAAAAAAAqo/U4x2LKpxxVcEqlq1iezVoPRU_AxrmQIFQCNcBGAsYHQ/s0/AV%2BThumbnail.webp`,
         type: `website`,
     },
     verification: {
