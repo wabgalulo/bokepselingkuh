6c6c70c6caf38130a3c33740983452887fbc04d7
isderific <164430536+isderific@users.noreply.github.com>
2024-03-22 06:19:57 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const font = Nunito({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: `${SITENAME} Video Bokep Indo Jepang Barat Viral Terbaru`,
     description: `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
-    metadataBase: new URL("https://bokepsange.pages.dev"),
+    metadataBase: new URL("https://bokepmenantu.pages.dev"),
     alternates: {
         canonical: `/`,
     },
@@ -21,8 +21,8 @@ export const metadata: Metadata = {
         type: `website`,
     },
     verification: {
-        google: 'NpayDIkiC7Ex2VkbGooVVr0AFkzwTeKfQDx-dORsCJw',
-        yandex: 'edec0f57827c0e66',
+        google: 'YYiuTu_zjs8Q9qSREZ-_O-y5elW_MiwAbbYAkPw2_b0',
+        yandex: '74e6377e53fe8cc3',
     },
 };
 
