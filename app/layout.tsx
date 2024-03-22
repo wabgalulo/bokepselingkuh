c12f34a3a7a984e26070a512e675ccdcb5d7b508
lapelive <161677466+lapelive@users.noreply.github.com>
2024-03-23 05:38:09 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const font = Nunito({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: `${SITENAME} Video Bokep Indo Viral Terbaru`,
     description: `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
-    metadataBase: new URL("https://bokeplive.pages.dev"),
+    metadataBase: new URL("https://bokepsex.pages.dev"),
     alternates: {
         canonical: `/`,
     },
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     },
     verification: {
         google: 'kAZh5T69CkuAZIls2sTDJy_nlGta_Ac5n3lbADWvCVU',
-        yandex: '6e346a42cf18829e',
+        yandex: 'acaad005dc005189',
     },
 };
 
