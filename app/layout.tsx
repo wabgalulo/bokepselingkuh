168a48e2cbec5f78ef2f9e31c293cad7151e3447
tuntugombe <164521912+tuntugombe@users.noreply.github.com>
2024-03-22 21:26:48 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const font = Nunito({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: `${SITENAME} Video Bokep Indo Jepang Barat Viral Terbaru`,
     description: `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
-    metadataBase: new URL("https://bocil-viral.pages.dev"),
+    metadataBase: new URL("https://bokepbacol.pages.dev"),
     alternates: {
         canonical: `/`,
     },
@@ -21,8 +21,8 @@ export const metadata: Metadata = {
         type: `website`,
     },
     verification: {
-        google: '93jn3q4wcNBXclqQiNpDplsnYAXBNbCh0I0E89Qyi4Y',
-        yandex: 'a8193418d6d0de25',
+        google: 'gvGbcZ79QJSTp5MQQXv4NcQqBIacNS1BtqxnPcR0LMg',
+        yandex: '9fca9373a26e9a58',
     },
 };
 
